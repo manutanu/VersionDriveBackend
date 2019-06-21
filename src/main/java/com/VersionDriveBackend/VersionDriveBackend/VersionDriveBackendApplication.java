3e4fd306ad121ec26d057237a475de7a5b96bc2d
Mritunjay Yadav <mritunjay.yadav@mindfiresolutions.com>
2019-06-21 22:56:58 +0530

    new project VersionDrive

---
+++ src/main/java/com/VersionDriveBackend/VersionDriveBackend/VersionDriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.VersionDriveBackend.VersionDriveBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionDriveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionDriveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VersionDriveBackend/VersionDriveBackend/VersionDriveBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.VersionDriveBackend.VersionDriveBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VersionDriveBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
